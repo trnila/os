f65a55a304a9413f978967b564f5e323958b4525
Daniel Trnka <daniel.trnka@gmail.com>
2014-11-15 00:11:27 +0100

    Added code for printing hexadecimal numbers

--- boot.s
+++ boot.s
@@ -5,6 +5,9 @@ loop:
 	mov bx, name
 	call printString
 
+	mov dx, 0x1fbf
+	call printHex
+
 	jmp $
 
 
@@ -31,6 +34,57 @@ printString:
 
 	printStringEnd:
 	ret
+
+
+; print hexa number to screen
+; dx - number to print
+printHex:
+	; set counter to address of first bit
+	mov bx, hexOut + 5
+
+	printHexNext:
+	; check if we are at end
+	cmp bx, hexOut + 1
+	jle printHexPrint
+
+	; create copy of number because we will destroy him
+	mov ax, dx
+
+	; get value of last position -> 1111 mask
+	and dx, 15
+
+	; if number is smaller than 9 - add '0'
+	cmp dx, 9
+	jle printHexNum
+	; otherwise add 'A'
+	add dx, 'A' - 10
+
+	printHexCont:
+	; save result to output string, dl - we need to write just lower 8bit!
+	mov [bx], dl
+
+	; obtain copy of dx from ax
+	mov dx, ax
+
+	; move to previous position of number
+	dec bx
+
+	; rotate right
+	shr dx, 4
+
+	jmp printHexNext
+
+	printHexPrint:
+	mov bx, hexOut
+	call printString
+	ret
+
+	printHexNum:
+	add dx, '0'
+	jmp printHexCont
+
+	; string that we are writting to
+	hexOut: db '0x0000', 0
 
 name:
 	db "Hello world!", 0
